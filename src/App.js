b0c1a0619723677df6947ca8c09e0ec6fd2d525d
t-shinkai00 <t.shinkai00@gmail.com>
2021-02-24 23:44:23 +0900

    add input and button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
   return (
     <div className="App">
       <h1>Hello World</h1>
+      {/* input feild */}
+      {/* button */}
+      {/* messages themselves */}
+      <input onChange={e=>setInput(e.target.value)}/>
+      <p>{input}</p>
+      <button>Send</button>
     </div>
   );
 }
